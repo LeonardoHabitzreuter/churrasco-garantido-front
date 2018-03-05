d70a9935941d9901cd0fefb9a70f536fcd7fab98
leonardo.h <leonardo.h@hbsis.com.br>
2018-03-05 12:03:02 -0300

    retornado componente de botao ao que era antes

--- src/components/button/index.js
+++ src/components/button/index.js
@@ -3,14 +3,9 @@ import { Button as ReactButton } from 'react-bootstrap'
 import styles from './button.styl'
 import classnames from 'classnames'
 
-const Button = ({ children, customStyle, ...rest }) => {
-  if (customStyle === '32163') empty()
-  return (
-    <ReactButton className={classnames(styles[rest.bsStyle], customStyle)} {...rest}>{children}</ReactButton>
-  )
-}
-
-const empty = () => {}
+const Button = ({ children, customStyle, ...rest }) => (
+  <ReactButton className={classnames(styles[rest.bsStyle], customStyle)} {...rest}>{children}</ReactButton>
+)
 
 Button.defaultProps = {
   bsStyle: 'primary',
