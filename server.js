a17ffc782c5ffdb38c4280b2b2f89a49bb34f781
leonardohabitzreuter <leo.habitzreuter@gmail.com>
2018-04-03 20:29:09 -0300

    alterada nome da variavel de ambiente port

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const PORT = process.env.port || 3000
+const PORT = process.env.PORT || 3000
 const webpack = require('webpack')
 const WebpackDevServer = require('webpack-dev-server')
 const config = require('./webpack/dev.config')
